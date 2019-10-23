c499ae1b49dcf381ff10f47748cbf4552e31b971
rnavarro <rod.nvrr@gmail.com>
2019-10-22 21:08:13 -0300

    /asset added w/js css files

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const express = require('express');
 
 const app = express();
 
+// Middleware
+// Callback o instruccion que se ejecutará siempre sin importa la url solicitada
+app.use(express.static(__dirname + '/public'));
+
 // Las peticiones que incluyan "/" pasan por el callback de la Arrow f(x)
-app.get('/', (req,res) => {
+// app.get('/', (req,res) => {
 
-    let out = {
-        name: 'name',
-        age: 32,
-        url: req.url
-    }
+//     let out = {
+//         name: 'name',
+//         age: 32,
+//         url: req.url
+//     }
 
-    //  res.send('Hello world ! ');
-    res.send(out);
+//     //  res.send('Hello world ! ');
+//     res.send(out);
     
-});
+// });
+
 
 app.get('/data',(req,res)=>{
     res.send('inside of "/data" ! ');
